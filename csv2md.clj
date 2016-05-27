227ce22428c0ad5ecb4943a21be25508ae4ef89a
John Gabriele <jgabriele@fastmail.fm>
2016-05-27 10:25:46 -0400

    rewritten

--- csv2md.clj
+++ csv2md.clj
@@ -15,55 +15,41 @@
 ;; You should have received a copy of the GNU General Public License
 ;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-'{:dependencies [[org.clojure/clojure "1.8.0"]
+'{:dependencies [[org.clojure/clojure  "1.8.0"]
                  [org.clojure/data.csv "0.1.3"]]}
 
-(require '[clojure.string   :as str]
-         '[clojure.data.csv :as csv])
+(require '[clojure.data.csv :as csv])
+(require '[clojure.string   :as str])
 
-(when (not= 1 (count *command-line-args*))
-  (println "Please pass exactly one arg: the csv filename.")
+(when (not= (count *command-line-args*) 1)
+  (println "Please pass exactly one arg, like so:
+
+    csv2md.clj filename.csv
+
+Pretty markdown table output will go to stdout.")
   (System/exit 0))
 
-(def infile-name (first *command-line-args*))
+(defn main
+  []
+  (let [input-file (first *command-line-args*)
+        rows (csv/read-csv (slurp input-file))
+        ;;_ (println rows)
+        inverted-rows (apply map vector rows)
+        ;;_ (println inverted-rows)
+        num-chars (for [r inverted-rows]
+                    (map count r))
+        ;;_ (println num-chars)
+        max-col-sizes (map (fn [c] (apply max c)) num-chars)
+        ;;_ (println max-col-sizes)
+        header (str/join "  " (for [n max-col-sizes]
+                                (str/join (repeat (+ n 2) "-"))))]
+    (println header)
+    (doseq [row rows]
+      (let [padded (for [[elem idx] (map vector row (range))]
+                     (format (str "%-" (+ 2 (nth max-col-sizes idx)) "s")
+                             elem))]
+        (println (str/join "  " padded))))
+    (println header)))
 
-(defn find-max-len
-  "Given a list of lists of strings, and also given an index
-(indicating which \"column\" you're interested in), find the
-max string length for all the strings in that column."
-  [coll col-num]
-  (let [these  (map (fn [row]
-                      (nth row col-num))
-                    coll)
-        counts (map count these)]
-    (apply max counts)))
-
-(defn print-guide-bars
-  "Prints out the md table delimiters. `max-lengths` is a map of
-col-num to that col-nums max size."
-  [max-lengths]
-  (doseq [[_ size] (sort max-lengths)]
-    (let [size (+ 2 size)]
-      (print (apply str (repeat size \-)))
-      (print "  ")))
-  (newline))
-  
-;; -----------------------------------------------------------------------
-(let [csv-data        (csv/read-csv (slurp infile-name))
-      num-cols        (count (first csv-data))
-      all-max-lengths (reduce (fn [accum i]
-                                (assoc accum i (find-max-len csv-data i)))
-                              {}
-                              (range num-cols))]
-  (dotimes [row-num (count csv-data)]
-    (when (= row-num 1)
-      (print-guide-bars all-max-lengths))
-    (dotimes [col-num num-cols]
-      (let [this-one     (nth (nth csv-data row-num)
-                              col-num)
-            max-col-size (+ 4 (get all-max-lengths col-num))
-            num-spaces   (- max-col-size (count this-one))
-            spaces       (apply str (repeat num-spaces \space))]
-        (print (str this-one spaces))))
-    (newline))
-  (print-guide-bars all-max-lengths))
+;; ------------------------
+(main)
